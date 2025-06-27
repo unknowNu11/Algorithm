9311bc5e15b331af6d05797ae46789352526373d
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-06-27 23:07:11 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 48 ms, Memory: 5380 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        while(true)
+        {
+            string n = Console.ReadLine();
+            Console.WriteLine(n);
+            
+            if(n == null) return;
+        }
+    }
+}
