378e9f90cc6284a2cfcca9a6062ba24d87d0278d
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-07-16 11:35:31 +0900

    [Bronze III] Title: 최댓값, Time: 48 ms, Memory: 5508 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.cs
+++ 백준/Bronze/2562. 최댓값/최댓값.cs
@@ -4,17 +4,16 @@ class Program
 {
     static void Main()
     {
-        string[] inputN = new string[9];
         int maxNum = 0;
         int N = 0;
         
         for(int a = 0; a < 9; a++)
         {
-            inputN[a] = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
             
-            if(maxNum < int.Parse(inputN[a]))
+            if(maxNum < number)
             {
-                maxNum = int.Parse(inputN[a]);
+                maxNum = number;
                 N = a + 1;
             }
         }
