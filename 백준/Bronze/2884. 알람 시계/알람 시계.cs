b5ffdca26f80636b62f5b71850eed84745ada32e
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-07-17 13:48:01 +0900

    [Bronze III] Title: 알람 시계, Time: 48 ms, Memory: 5524 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.cs
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cs
@@ -4,28 +4,11 @@ class Program
 {
     static void Main()
     {
-        string[] input = Console.ReadLine().Split();
-        
-        if(int.Parse(input[1]) >= 45)
-        {
-            Console.Write(int.Parse(input[0]));
-            Console.Write(" ");
-            Console.Write(int.Parse(input[1]) - 45);
-        }
-        else
-        {
-            if(int.Parse(input[0]) > 0)
-            {
-                Console.Write(int.Parse(input[0]) - 1);
-                Console.Write(" ");
-                Console.Write(int.Parse(input[1]) + 60 - 45);
-            }
-            else
-            {
-                Console.Write("23");
-                Console.Write(" ");
-                Console.Write(int.Parse(input[1]) + 60 - 45);
-            }
-        }
+        int[] time = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        int total = (time[0] * 60 + time[1]) - 45;
+        if (total < 0) total += 24 * 60;
+
+        Console.WriteLine($"{total / 60} {total % 60}");
     }
 }
