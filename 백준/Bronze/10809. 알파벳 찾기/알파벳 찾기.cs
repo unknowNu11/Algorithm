cd97fa8a3f49bbd682537e6edfabf70527b876cf
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-07-24 20:30:01 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 52 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+            string s = Console.ReadLine();
+
+            for (int i = 97; i < 123; i++)
+            {
+                    Console.Write(s.IndexOf(Convert.ToChar(i)));
+                    Console.Write(" ");
+
+            }
+    }
+}
