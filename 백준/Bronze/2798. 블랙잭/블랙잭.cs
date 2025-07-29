6948686c456994ffe41dea1f186b83d973e894ed
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-07-29 23:25:27 +0900

    [Bronze II] Title: 블랙잭, Time: 52 ms, Memory: 5968 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cs
@@ -0,0 +1,41 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+
+        string[] input2 = Console.ReadLine().Split();
+
+        int count = int.Parse(input[0]);
+
+        int input_val = int.Parse(input[1]);
+
+        int sum = -1;
+        int min = 0;
+        int[] num = new int[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            num[i] = int.Parse(input2[i]);
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = i + 1; j < count; j++)
+            {
+                for (int k = j + 1; k < count; k++)
+                {
+                    sum = num[i] + num[j] + num[k];
+
+                    if(sum <=input_val && min < sum)
+                    {
+                        min = sum;
+                    }
+                }
+            }
+        }
+        Console.Write(min);
+    }
+}
