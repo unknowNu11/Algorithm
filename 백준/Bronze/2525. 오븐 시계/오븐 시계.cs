3b5b28203342a4a8ea30cd331a75dbebde873433
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-08-02 16:28:53 +0900

    [Bronze III] Title: 오븐 시계, Time: 52 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] s = Console.ReadLine().Split();
+        int h = int.Parse(s[0]), m = int.Parse(s[1]) + int.Parse(Console.ReadLine());
+
+        h = (h + m / 60) % 24;
+        m %= 60;
+
+        Console.WriteLine($"{h} {m}");
+    }
+}
