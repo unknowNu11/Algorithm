da66140ea191b0142791920b671978ba0ae42ef2
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-08-04 15:37:59 +0900

    [Bronze V] Title: 합, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+        
+        Console.WriteLine(sum);
+    }
+}
