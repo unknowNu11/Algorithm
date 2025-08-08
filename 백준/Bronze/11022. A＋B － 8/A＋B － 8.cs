2d17c92f18353908aff45543d9e58191e4f6a240
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-08-08 10:54:17 +0900

    [Bronze V] Title: A+B - 8, Time: 56 ms, Memory: 6212 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        for(int i = 1; i <= n; i++)
+        {
+            int sum = 0;
+            string[] input = Console.ReadLine().Split();
+            
+            int n1 = int.Parse(input[0]);
+            int n2 = int.Parse(input[1]);    
+                
+            sum = n1 + n2;
+            
+            Console.WriteLine($"Case #{i}: {n1} + {n2} = {sum}");
+        }
+    }
+}
