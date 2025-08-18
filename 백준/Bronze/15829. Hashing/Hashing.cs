d5d23b47b564bcac844fa8c4e54510454d06ff59
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-08-18 21:53:13 +0900

    [Bronze II] Title: Hashing, Time: 52 ms, Memory: 5492 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cs
+++ 백준/Bronze/15829. Hashing/Hashing.cs
@@ -4,15 +4,17 @@ class Program
 {
     static void Main()
     {
-        double hashValue = 0;
         int length = int.Parse(Console.ReadLine());
         string str = Console.ReadLine();
         
         int M = 1234567891;
+        long hashValue = 0;
+        long pow = 1;
         
         for(int i = 0; i < length; i++)
         {
-            hashValue = (hashValue + (str[i] - 'a' + 1) * Math.Pow(31, i)) % M; 
+            hashValue = (hashValue + (str[i] - 'a' + 1) * pow) % M; 
+            pow = (pow * 31) % M;
         }
         
         Console.WriteLine(hashValue);
