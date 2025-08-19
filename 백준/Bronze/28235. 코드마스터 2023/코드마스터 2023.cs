ec2340d66978f5b26ee90d25105fc3e659d94d72
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-08-19 11:48:24 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 48 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        
+        switch(input)
+        {
+            case "SONGDO":
+                Console.WriteLine("HIGHSCHOOL");
+                break;
+                
+            case "CODE":
+                Console.WriteLine("MASTER");
+                break;
+                
+            case "2023":
+                Console.WriteLine("0611");
+                break;
+                
+            case "ALGORITHM":
+                Console.WriteLine("CONTEST");
+                break;
+        }
+    }
+}
