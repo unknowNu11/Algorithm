52891ecddd6c8b5259fdbf7a9c4af620110a984f
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-08-22 08:39:46 +0900

    [Silver IV] Title: 스택, Time: 484 ms, Memory: 7732 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cs
@@ -0,0 +1,42 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int count = int.Parse(Console.ReadLine());
+        Stack<int> stack = new Stack<int>();
+
+        for(int i = 0; i < count; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            switch(input[0])
+            {
+                case "push":
+                    stack.Push(int.Parse(input[1]));
+                    break;
+                case "pop":
+                    if(stack.Count > 0)
+                        Console.WriteLine(stack.Pop());
+                    else
+                        Console.WriteLine("-1");
+                    break;
+                case "size":
+                    Console.WriteLine(stack.Count);
+                    break;
+                case "empty":
+                    Console.WriteLine(stack.Count == 0 ? "1" : "0");
+                    break;
+                case "top":
+                    if(stack.Count > 0)
+                        Console.WriteLine(stack.Peek());
+                    else
+                        Console.WriteLine("-1");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
