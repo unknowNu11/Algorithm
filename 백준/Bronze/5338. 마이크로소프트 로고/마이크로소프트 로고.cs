bfe0f232fac8bf591243e8edf9206a0a27f69ae9
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-09-01 18:59:03 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 44 ms, Memory: 5304 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,6 @@
+// 최상위문, 실제론 사용 지양
+Console.WriteLine("       _.-;;-._");
+Console.WriteLine("'-..-'|   ||   |");
+Console.WriteLine("'-..-'|_.-;;-._|");
+Console.WriteLine("'-..-'|   ||   |");
+Console.WriteLine("'-..-'|_.-''-._|");
