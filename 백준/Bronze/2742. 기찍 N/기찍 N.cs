eebbe686152397a0415f6b31483df2c06b694d06
DDUKKIJWIG <102303477+DDUKKIJWIG@users.noreply.github.com>
2025-09-03 20:23:13 +0900

    [Bronze V] Title: 기찍 N, Time: 120 ms, Memory: 10932 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+StringBuilder sb = new StringBuilder();
+
+for(int i = n; i > 0; i--)
+{
+    sb.AppendLine(i.ToString());
+}
+
+Console.Write(sb);
