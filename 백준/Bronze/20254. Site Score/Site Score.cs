8f8f4850666738547eff9f695300387665745e08
Oh Seung Min <102303477+unknowNu11@users.noreply.github.com>
2026-03-19 23:12:44 +0900

    [Bronze V] Title: Site Score, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cs
@@ -0,0 +1,7 @@
+string[] inputs = Console.ReadLine().Split(' ');
+int a = int.Parse(inputs[0]);
+int b = int.Parse(inputs[1]);
+int c = int.Parse(inputs[2]);
+int d = int.Parse(inputs[3]);
+
+Console.WriteLine( 56 * a + 24 * b + 14 * c + 6 * d );
