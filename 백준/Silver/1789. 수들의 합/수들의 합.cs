e531c6a370ef2d6f0ba562589c0077bf906f1191
Oh Seung Min <102303477+unknowNu11@users.noreply.github.com>
2026-03-22 01:45:32 +0900

    [Silver V] Title: 수들의 합, Time: 48 ms, Memory: 5512 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.cs
+++ 백준/Silver/1789. 수들의 합/수들의 합.cs
@@ -2,7 +2,7 @@ long total = 0;
 long count = 0;
 long input = long.Parse(Console.ReadLine());
 
-while (total + count + 1 <= input)
+while (total + count < input)
 {
     count++;
     total += count;
